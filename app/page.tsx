d39aeded91fbd290e7ad06dbec88b71c4b5fa64a
onlyeugene <eugeneokogun@gmail.com>
2024-11-18 15:26:25 +0100

    feat: fixed responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <>
     <div className="sm:pt-40 pt-24">
-
       <MoviesList />
+
+      <h1 className="text-center text-sm sm:text-base md:text-lg font-medium mt-4 sm:mt-6 md:mt-8 mb-3">
+        &copy; {new Date().getFullYear()} All rights reserved.
+      </h1>
     </div>
     </>
   );
